c58b8fd119e3755dfcd362ee6b3df17bc588fb7f
WilliamDouris <william.douris@me.com>
2021-02-20 12:04:43 +0100

    Update pytwitch.py

--- pytwitch.py
+++ pytwitch.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
+import time
 
 class pytwitch:
     browser = None
@@ -36,21 +37,27 @@ class pytwitch:
         if str_channel is not None:
             self.browser.get('https://www.twitch.com/' + str_channel)
 
-        # TODO : May add verification of url or page to make sure we are on channel page
-
         if not self.is_page_exist():
-            print("get_channel_status() - The channel name don't exist")
+            raise ValueError("The channel don't exist")
+
+        if self.is_live():
+            print("is live")
+            return 1
+        else:
+            print("is not live")
             return 0
 
+    def is_live(self):
         try:
-            if twitch_obj.browser.find_element_by_class_name("tw-strong tw-upcase tw-white-space-nowrap").text is "LIVE": # Overkill, I think we don't need that
-                return 0
-        except NoSuchElementException:
+            #self.browser.find_element_by_class_name("live-indicator-container tw-border-radius-large tw-inline")
+            self.browser.find_element_by_xpath("//p[@class='live-indicator-container tw-border-radius-large tw-inline']")
             return 1
+        except NoSuchElementException:
+            return 0
 
     def is_page_exist(self):
         try:
-            self.browser.find_element_by_class_name("core-error__message-container tw-flex tw-flex-column")
+            self.browser.find_element_by_xpath("//p[@data-a-target='core-error-message']")
             return 0
         except NoSuchElementException:
             return 1
@@ -72,7 +79,8 @@ class pytwitch:
         pass
 
     def write_chat(self, message):
-        pass
+        chat_input = self.browser.find_element_by_xpath("//textarea[@data-a-target='chat-input']")
+        chat_input.send_keys(message)
 
     def get_chat(self, contain=None, not_contain=None):
         pass
@@ -92,6 +100,13 @@ class pytwitch:
 
 
 if __name__ == "__main__":
-    twitch_obj = twitch()
+    twitch_obj = pytwitch()
     twitch_obj.watch_channel("skyyart")
-    print()
+    time.sleep(5)
+    try:
+        twitch_obj.browser.find_element_by_xpath("//p[@class='live-indicator-container tw-border-radius-large tw-inline']")
+        print('ok')
+    except:
+        pass
+    print("let's go ")
+    twitch_obj.browser.find_element_by_class_name("tw-strong tw-upcase tw-white-space-nowrap")
